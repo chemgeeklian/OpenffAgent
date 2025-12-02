3a103007732b2e5a1809e40088d7e57d99734d55
chemgeeklian <xinran.lian@gmail.com>
2025-12-02 00:16:27 +0000

    can load from smiles from rxn and fixed ptm pdb

--- notebooks/debug_kpi.py
+++ notebooks/debug_kpi.py
@@ -1,12 +1,13 @@
-pdb_path = '/eagle/projects/FoundEpidem/xlian/Agent/OpenffAgent/tmp/ptm_H.pdb'
-kpi_smiles_path = '/eagle/projects/FoundEpidem/xlian/Agent/OpenffAgent/tmp/predmodel_KPI_smiles.txt'
+unfixed_pdb_path = '/eagle/projects/FoundEpidem/xlian/Agent/OpenffAgent/tmp/ptm/pred.model_idx_0.cif'
+fixed_pdb_path = '/eagle/projects/FoundEpidem/xlian/Agent/OpenffAgent/tmp/ptm/pred.model_idx_0_fixed.pdb'
 
-from openff.toolkit import Molecule, Topology
-from openmm.app import PDBFile, Modeller, ForceField as OpenMMForceField
-from pdbfixer import PDBFixer
-import rdkit
+kpi_smiles_path = '/eagle/projects/FoundEpidem/xlian/Agent/OpenffAgent/tmp/noptm_H_add_KPI_smiles.txt'
+
+from openff.toolkit import Molecule
+from openmm.app import PDBFile
 from rdkit import Chem
-import os
+from pdbfixer import PDBFixer
+import openff_agent.utils as utils
 
 # get smiles from txt
 smiles = open(kpi_smiles_path, 'r').read()
@@ -14,11 +15,15 @@ smiles = open(kpi_smiles_path, 'r').read()
 print('number of atoms in smiles: ')
 print(len(Chem.AddHs(Chem.MolFromSmiles(smiles)).GetAtoms()))
 
-pdb = PDBFile(pdb_path)
+utils.fix_pdb(unfixed_pdb_path, fixed_pdb_path, resid_to_rm_atom={'126': ['HO1', 'H2']})
+pdb = PDBFile(fixed_pdb_path)
+
 num_atoms = sum(1 for _ in pdb.topology.atoms())
 print("number of atoms in PDB:", num_atoms)
 
-mol_withKPI_from_smiles = Molecule.from_pdb_and_smiles(pdb_path, smiles, allow_undefined_stereo=True)
+mol_withKPI_from_smiles = Molecule.from_pdb_and_smiles(fixed_pdb_path, smiles, allow_undefined_stereo=True)
 print(f"✅ Loaded molecule with from_pdb_and_smiles()")
 
 # diff protein_with_KPI.pdb /eagle/projects/FoundEpidem/xlian/bio_ai_agent_genslm_example/energy_minim/openff_test/input/2v82_protein_with_H.pdb > diff.diff
+
+# can load after removing H2 and HO1 from KPI
