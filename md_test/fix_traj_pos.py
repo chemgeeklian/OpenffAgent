5be673eda546b0de7f122122471ae67dad0353fe
chemgeeklian <xinran.lian@gmail.com>
2026-02-23 17:17:06 +0000

    able to runcoord fix of a list

--- md_test/fix_traj_pos.py
+++ md_test/fix_traj_pos.py
@@ -1,53 +1,43 @@
 import mdtraj as md
 import numpy as np
+import argparse
+import glob
+import os
 
-'''
-This script recenters a molecular dynamics trajectory on the protein's center of mass (COM).
-It performs the following steps:
-1) Loads the trajectory and topology.
-2) Images the molecules to account for periodic boundary conditions.
-3) Recenters each frame on the protein's COM.
-4) Saves the modified trajectory and a PDB of the first frame.
-'''
 
-pdb = "../tmp/minimized.pdb"
-dcd = "../md_test/run/eq.dcd"
-output_dcd_path = "../md_test/run/eq_centered.dcd"
+def process_run_dir(run_dir):
+    pdb = os.path.join(run_dir, "minimized.pdb")
+    dcd = os.path.join(run_dir, "prod.dcd")
+    out_dcd = os.path.join(run_dir, "prod_centered.dcd")
+    # out_pdb = os.path.join(run_dir, "prod_frame0_centered.pdb")
 
-print("Loading trajectory...")
-traj = md.load(dcd, top=pdb)
+    traj = md.load(dcd, top=pdb)
+    protein_idx = traj.topology.select("protein")
+    traj.image_molecules(inplace=True)
 
-# Select protein atoms
-protein_idx = traj.topology.select("protein")
-if protein_idx.size == 0:
-    raise RuntimeError("No protein atoms found (topology issue?).")
+    # 2) recenter to protein COM per frame
+    print("Centering on protein COM...")
+    for i, frame in enumerate(traj):
+        xyz = frame.xyz[0]
+        protein_xyz = xyz[protein_idx]
+        com = protein_xyz.mean(axis=0)
 
-print(f"Protein atoms: {protein_idx.size}")
+        # use box center when available, otherwise center at origin
+        box_lengths = frame.unitcell_lengths[0] if frame.unitcell_lengths is not None else None
+        has_box = box_lengths is not None and np.any(box_lengths > 0)
+        if has_box:
+            box_center = box_lengths / 2.0
+            shift = box_center - com
+        else:
+            shift = -com
 
-# 1) initial imaging
-print("Imaging molecules...")
-traj.image_molecules(inplace=True)
+        frame.xyz[0] = xyz + shift
 
-# 2) recenter to protein COM
-print("Centering on protein COM...")
-for i, frame in enumerate(traj):
-    xyz = frame.xyz[0]  # shape = (n_atoms, 3)
-    
-    protein_xyz = xyz[protein_idx]
-    com = protein_xyz.mean(axis=0)
+    traj.image_molecules(inplace=True)
+    traj.save(out_dcd)
 
-    box = frame.unitcell_lengths[0]  # (Lx, Ly, Lz)
-    box_center = box / 2.0
 
-    shift = box_center - com
-    frame.xyz[0] = xyz + shift
-
-# 3) wrap again
-print("Final wrapping...")
-traj.image_molecules(inplace=True)
-
-# save
-traj.save("../md_test/run/eq_centered.dcd")
-traj[0].save_pdb("../md_test/run/eq_frame0_centered.pdb")
-
-print("Saved eq_centered.dcd and eq_frame0_centered.pdb")
+if __name__ == "__main__":
+    for i in range(1,5):
+        run_dir = f"../runs_with_equil/ec03_1_runs/run_{i}/run"
+        process_run_dir(run_dir)
